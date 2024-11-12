749f2de11063363e5c044c6e271e24fb7f801182
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-11-12 00:45:55 -0600

    wip

--- feeds/test/madison_clojure/rsvp_test.clj
+++ feeds/test/madison_clojure/rsvp_test.clj
@@ -16,12 +16,12 @@
 
 (deftest add-rsvps-to-event-test
   (is (= [{:summary "1", :uid "1", :rsvp "https://github.com/orgs/madclj/discussions/6",
-           :description "desc1\n\nAttendees:\n- [Ambrose Bonnaire-Sergeant](https://github.com/frenchy64)"
+           :description "desc1\n\nAttendees: 1"
            :attendees [{:name "Ambrose Bonnaire-Sergeant",
                         :url "https://github.com/frenchy64",
                         :avatar-url "https://avatars.githubusercontent.com/u/287396?u=2aa22e9ddcc23256939aa36dbd3ca60f3e260e69&v=4"}]}
           {:summary "2", :uid "2", :rsvp "https://github.com/orgs/madclj/discussions/7",
-           :description "desc2\n\nAttendees:\n- [User 2](https://github.com/user2)"
+           :description "desc2\n\nAttendees: 1"
            :attendees [{:name "User 2",
                         :url "https://github.com/user2",
                         :avatar-url "avatar-url"}]}]
