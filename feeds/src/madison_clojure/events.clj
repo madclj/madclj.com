bc605799a01e3200f41391145240fbe1f5cd9cbe
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-12-11 23:23:46 -0600

    2025 events

--- feeds/src/madison_clojure/events.clj
+++ feeds/src/madison_clojure/events.clj
@@ -66,6 +66,126 @@
     :location startingblock
     :start (madison-time "2024-12-11T18:30")
     :end   (madison-time "2024-12-11T21:00")}
+   {:full-title "January Meetup (TBD)"
+    :summary "January Meetup (TBD)"
+    :uid "https://github.com/orgs/madclj/discussions/10"
+    :description
+    (text
+      "TBD")
+    :rsvp "https://github.com/orgs/madclj/discussions/10"
+    :location startingblock
+    :start (madison-time "2025-01-08T18:30")
+    :end   (madison-time "2025-01-08T21:00")}
+   {:full-title "February Meetup (TBD)"
+    :summary "February Meetup (TBD)"
+    :uid "https://github.com/orgs/madclj/discussions/11"
+    :description
+    (text
+      "TBD")
+    :rsvp "https://github.com/orgs/madclj/discussions/11"
+    :location startingblock
+    :start (madison-time "2025-02-12T18:30")
+    :end   (madison-time "2025-02-12T21:00")}
+   {:full-title "March Meetup (TBD)"
+    :summary "March Meetup (TBD)"
+    :uid "https://github.com/orgs/madclj/discussions/12"
+    :description
+    (text
+      "TBD")
+    :rsvp "https://github.com/orgs/madclj/discussions/12"
+    :location startingblock
+    :start (madison-time "2025-03-12T18:30")
+    :end   (madison-time "2025-03-12T21:00")}
+   {:full-title "April Meetup (TBD)"
+    :summary "April Meetup (TBD)"
+    :uid "https://github.com/orgs/madclj/discussions/13"
+    :description
+    (text
+      "TBD")
+    :rsvp "https://github.com/orgs/madclj/discussions/13"
+    :location startingblock
+    :start (madison-time "2025-04-09T18:30")
+    :end   (madison-time "2025-04-09T21:00")}
+   {:full-title "May Meetup (TBD)"
+    :summary "May Meetup (TBD)"
+    :uid "https://github.com/orgs/madclj/discussions/14"
+    :description
+    (text
+      "TBD")
+    :rsvp "https://github.com/orgs/madclj/discussions/14"
+    :location startingblock
+    :start (madison-time "2025-05-14T18:30")
+    :end   (madison-time "2025-05-14T21:00")}
+   {:full-title "June Meetup (TBD)"
+    :summary "June Meetup (TBD)"
+    :uid "https://github.com/orgs/madclj/discussions/15"
+    :description
+    (text
+      "TBD")
+    :rsvp "https://github.com/orgs/madclj/discussions/15"
+    :location startingblock
+    :start (madison-time "2025-06-11T18:30")
+    :end   (madison-time "2025-06-11T21:00")}
+   {:full-title "July Meetup (TBD)"
+    :summary "July Meetup (TBD)"
+    :uid "https://github.com/orgs/madclj/discussions/16"
+    :description
+    (text
+      "TBD")
+    :rsvp "https://github.com/orgs/madclj/discussions/16"
+    :location startingblock
+    :start (madison-time "2025-07-09T18:30")
+    :end   (madison-time "2025-07-09T21:00")}
+   {:full-title "August Meetup (TBD)"
+    :summary "August Meetup (TBD)"
+    :uid "https://github.com/orgs/madclj/discussions/17"
+    :description
+    (text
+      "TBD")
+    :rsvp "https://github.com/orgs/madclj/discussions/17"
+    :location startingblock
+    :start (madison-time "2025-08-13T18:30")
+    :end   (madison-time "2025-08-13T21:00")}
+   {:full-title "September Meetup (TBD)"
+    :summary "September Meetup (TBD)"
+    :uid "https://github.com/orgs/madclj/discussions/18"
+    :description
+    (text
+      "TBD")
+    :rsvp "https://github.com/orgs/madclj/discussions/18"
+    :location startingblock
+    :start (madison-time "2025-09-10T18:30")
+    :end   (madison-time "2025-09-10T21:00")}
+   {:full-title "October Meetup (TBD)"
+    :summary "October Meetup (TBD)"
+    :uid "https://github.com/orgs/madclj/discussions/19"
+    :description
+    (text
+      "TBD")
+    :rsvp "https://github.com/orgs/madclj/discussions/19"
+    :location startingblock
+    :start (madison-time "2025-10-08T18:30")
+    :end   (madison-time "2025-10-08T21:00")}
+   {:full-title "November Meetup (TBD)"
+    :summary "November Meetup (TBD)"
+    :uid "https://github.com/orgs/madclj/discussions/20"
+    :description
+    (text
+      "TBD")
+    :rsvp "https://github.com/orgs/madclj/discussions/20"
+    :location startingblock
+    :start (madison-time "2025-11-12T18:30")
+    :end   (madison-time "2025-11-12T21:00")}
+   {:full-title "December Meetup (TBD)"
+    :summary "December Meetup (TBD)"
+    :uid "https://github.com/orgs/madclj/discussions/21"
+    :description
+    (text
+      "TBD")
+    :rsvp "https://github.com/orgs/madclj/discussions/21"
+    :location startingblock
+    :start (madison-time "2025-12-10T18:30")
+    :end   (madison-time "2025-12-10T21:00")}
    ])
 
 (assert (every? :uid events))
